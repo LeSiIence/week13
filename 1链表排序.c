463bc7f05d486eee8b8a9a6115236ff25cbbdde3
admin <admin@5PRO>
2024-11-28 19:21:46 +0800

    添加项目文件。

---
+++ 1链表排序.c
@@ -0,0 +1,22 @@
+//链表排序
+#define _CRT_SECURE_NO_WARINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+int main()
+{
+	
+	return 0;
+}
+
+typedef struct Node
+
+{
+
+	int data;//存放数据
+
+	struct Node* pNext;//指向下一个结构体对象
+
+}NODE, * pNODE;
